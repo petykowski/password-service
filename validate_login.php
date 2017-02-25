d10b97fd3f60e3bfb9b5dcf63f7b1e29890929cf
Sean Petykowski <spetykowski@gmail.com>
2017-02-25 16:46:26 -0500

    Support For New User Sign Up
    This commit implements the ability to sign up as a new user, and also
    includes some safety checks. The checks occur on the php side to ensure
    username and password are populated, not a duplicate of what is already
    in the system, and a confirmation that the user has been created.
    
    Additionally, the php file is used in sync with both the HTML site and
    Android application.

--- validate_login.php
+++ validate_login.php
@@ -3,24 +3,47 @@
 // Access provided by database configuration file
 require_once 'config.php';
 
-$form_username = $_POST["username"];
-$form_password = $_POST["password"];
+//connection to the database
+$con = new mysqli($servername, $sqlusername, $sqlpassword, $sqldbname)
+	or die("Unable to connect to MySQL");
 
-foreach($_POST as $name => $content) { // Most people refer to $key => $value
-   echo "The HTML name: $name <br>";
-   echo "The content of it: $content <br>";
+if (isset($_POST['signup-button']))   {
+    $signup_username = $_POST["signup-username"];
+	$signup_password = $_POST["signup-password"];
+	
+	if(empty($signup_username) || empty($signup_password)) {
+		echo "Username and password cannot be empty";
+	} else {
+		$check = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
+		$check_row = mysqli_fetch_array($check);
+		if($check_row["Username"] == $signup_username) {
+			echo "Username already exsists, please use different username.";
+		} else {
+			$insert = mysqli_query($con,"INSERT INTO table1 (Username, Password, Role) VALUES ('$signup_username', '$signup_password','user')");
+			$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
+			$row = mysqli_fetch_array($result);
+	
+			if($row["Username"] == $signup_username) {
+				echo "Your account was created successfully.";
+			} else {
+				echo "Unable to create your account, Please try again.";
+			}
+		}
+	}	
+} elseif (!empty($_POST["username"])) {
+	$form_username = $_POST["username"];
+	$form_password = $_POST["password"];
+	
+    // Select the database to use
+	$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$form_username'");
+	$row = mysqli_fetch_array($result);
+
+	if($row["Username"] == $form_username && $row["Password"] == $form_password)
+		echo "You are a validated user.";
+	else
+		echo "Sorry, your credentials are not valid, Please try again.";
+} else {
+    echo "No data provided.";
 }
-   //connection to the database
-   $con = new mysqli($servername, $sqlusername, $sqlpassword, $sqldbname)
-     or die("Unable to connect to MySQL");
-     
-// Select the database to use
-$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$form_username'");
 
-$row = mysqli_fetch_array($result);
-
-if($row["Username"]==$form_username && $row["Password"]== $form_password)
-   echo"You are a validated user.";
-else
-   echo"Sorry, your credentials are not valid, Please try again.";
 ?>
