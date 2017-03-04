945d18ccf25d6f46374aba047ce4e730c37d2649
Sean Petykowski <spetykowski@gmail.com>
2017-03-04 12:21:31 -0500

    JSON Response
    When POSTing against validate_login.php the response will now come in
    the form of a JSON file. Additionally, there are multiple actions
    available when POSTing.
    
    * validate_username
    * signup
    * login

--- validate_login.php
+++ validate_login.php
@@ -1,49 +1,67 @@
 <?php
 
+// Prepare for JSON Response
+header('Content-Type: application/json');
+
 // Access provided by database configuration file
 require_once 'config.php';
 
-//connection to the database
+//Connect to Database
 $con = new mysqli($servername, $sqlusername, $sqlpassword, $sqldbname)
 	or die("Unable to connect to MySQL");
 
-if (isset($_POST['signup-button']))   {
-    $signup_username = $_POST["signup-username"];
-	$signup_password = $_POST["signup-password"];
-	
-	if(empty($signup_username) || empty($signup_password)) {
-		echo "Username and password cannot be empty";
-	} else {
+switch ($_POST["action"]) {
+	case "validate_username":
+		$signup_username = $_POST["signup-username"];
 		$check = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
 		$check_row = mysqli_fetch_array($check);
 		if($check_row["Username"] == $signup_username) {
-			echo "Username already exsists, please use different username.";
+			$arr = array('response' => 'Failure', 'message' => 'Username already exsists, please use different username.');
+			echo json_encode($arr);
+		}
+		break;
+	case "signup":
+		$signup_username = $_POST["signup-username"];
+		$signup_password = $_POST["signup-password"];
+
+		if(empty($signup_username) || empty($signup_password)) {
+			echo "Username and password cannot be empty";
 		} else {
-			$insert = mysqli_query($con,"INSERT INTO table1 (Username, Password, Role) VALUES ('$signup_username', '$signup_password','user')");
-			$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
-			$row = mysqli_fetch_array($result);
-	
-			if($row["Username"] == $signup_username) {
-				echo "Your account was created successfully.";
+			$check = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
+			$check_row = mysqli_fetch_array($check);
+			if($check_row["Username"] == $signup_username) {
+				$arr = array('response' => 'Failure', 'message' => 'Username already exsists, please use different username.');
+				echo json_encode($arr);
 			} else {
-				echo "Unable to create your account, Please try again.";
+				$insert = mysqli_query($con,"INSERT INTO table1 (Username, Password, Role) VALUES ('$signup_username', '$signup_password','user')");
+				$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
+				$row = mysqli_fetch_array($result);
+
+				if($row["Username"] == $signup_username) {
+					$arr = array('response' => 'Success', 'message' => 'Your account was created successfully.');
+					echo json_encode($arr);
+				} else {
+					$arr = array('response' => 'Failure', 'message' => 'Unable to create your account, Please try again.');
+					echo json_encode($arr);
+				}
 			}
 		}
-	}	
-} elseif (!empty($_POST["username"])) {
-	$form_username = $_POST["username"];
-	$form_password = $_POST["password"];
-	
-    // Select the database to use
-	$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$form_username'");
-	$row = mysqli_fetch_array($result);
+			break;
+	case "login":
+		$form_username = $_POST["username"];
+		$form_password = $_POST["password"];
+		
+		// Select the database to use
+		$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$form_username'");
+		$row = mysqli_fetch_array($result);
 
-	if($row["Username"] == $form_username && $row["Password"] == $form_password)
-		echo "You are a validated user.";
-	else
-		echo "Sorry, your credentials are not valid, Please try again.";
-} else {
-    echo "No data provided.";
+		if ($row["Username"] == $form_username && $row["Password"] == $form_password) {
+			$arr = array('response' => 'Success', 'message' => 'You are a validated user.');
+			echo json_encode($arr);
+		} else {
+			$arr = array('response' => 'Failure', 'message' => 'Sorry, your credentials are not valid, Please try again.');
+			echo json_encode($arr);
+		}
+		break;
 }
-
 ?>
