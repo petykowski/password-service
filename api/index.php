f5dedb266fb681400e53bb0eb5e5db2bd6d81c6c
Sean Petykowski <spetykowski@gmail.com>
2017-03-06 21:37:51 -0500

    Password Hashing and API
    This commit implements PHP's password_hash library to hash and verify
    user passwords. Changes were also made to database to reflect these
    changes. Additionally, file validate_login.php was updated to index.php
    at /api/.

--- api/index.php
+++ api/index.php
@@ -4,15 +4,15 @@
 header('Content-Type: application/json');
 
 // Access provided by database configuration file
-require_once 'config.php';
+require_once dirname(__DIR__).'/config.php';
 
 //Connect to Database
 $con = new mysqli($servername, $sqlusername, $sqlpassword, $sqldbname)
 	or die("Unable to connect to MySQL");
-
+	
 switch ($_POST["action"]) {
 	case "validate_username":
-		$signup_username = $_POST["signup-username"];
+		$signup_username = $_POST["username"];
 		$check = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
 		$check_row = mysqli_fetch_array($check);
 		if($check_row["Username"] == $signup_username) {
@@ -21,11 +21,12 @@ switch ($_POST["action"]) {
 		}
 		break;
 	case "signup":
-		$signup_username = $_POST["signup-username"];
-		$signup_password = $_POST["signup-password"];
+		$signup_username = $_POST["username"];
+		$signup_password = $_POST["password"];
 
 		if(empty($signup_username) || empty($signup_password)) {
-			echo "Username and password cannot be empty";
+			$arr = array('response' => 'Failure', 'message' => 'Username and password cannot be empty.');
+			echo json_encode($arr);
 		} else {
 			$check = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
 			$check_row = mysqli_fetch_array($check);
@@ -33,7 +34,8 @@ switch ($_POST["action"]) {
 				$arr = array('response' => 'Failure', 'message' => 'Username already exsists, please use different username.');
 				echo json_encode($arr);
 			} else {
-				$insert = mysqli_query($con,"INSERT INTO table1 (Username, Password, Role) VALUES ('$signup_username', '$signup_password','user')");
+				$hashpw = password_hash($signup_password, PASSWORD_DEFAULT);
+				$insert = mysqli_query($con,"INSERT INTO table1 (Username, Password, Role) VALUES ('$signup_username', '$hashpw','user')");
 				$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
 				$row = mysqli_fetch_array($result);
 
@@ -50,12 +52,12 @@ switch ($_POST["action"]) {
 	case "login":
 		$form_username = $_POST["username"];
 		$form_password = $_POST["password"];
-		
+	
 		// Select the database to use
 		$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$form_username'");
 		$row = mysqli_fetch_array($result);
 
-		if ($row["Username"] == $form_username && $row["Password"] == $form_password) {
+		if ($row["Username"] == $form_username && password_verify($form_password, $row["Password"])) {
 			$arr = array('response' => 'Success', 'message' => 'You are a validated user.');
 			echo json_encode($arr);
 		} else {
