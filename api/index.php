d1fec30f3c0011afb21bc147a92267aae694ee5e
Sean Petykowski <spetykowski@gmail.com>
2017-03-07 22:19:30 -0500

    Sanitize and Lowercase Usernames
    Closes #2
    
    This commit optimizes the user validation and creation code to first
    lowercase usernames then sanitize the username by using the
    FILTER_SANITIZE_EMAIL php filter. This commit also contains
    inline documentation of code.

--- api/index.php
+++ api/index.php
@@ -6,58 +6,80 @@ header('Content-Type: application/json');
 // Access provided by database configuration file
 require_once dirname(__DIR__).'/config.php';
 
+// Format and Sanitize Username
+$username = strtolower($_POST["username"]);
+$sanitized_username = filter_var($username, FILTER_SANITIZE_EMAIL);
+
+$password = $_POST["password"];
+
 //Connect to Database
 $con = new mysqli($servername, $sqlusername, $sqlpassword, $sqldbname)
 	or die("Unable to connect to MySQL");
 	
 switch ($_POST["action"]) {
+	/**
+	* Will check the provided signup username against the user database to check for exsisting entries.
+	*/
 	case "validate_username":
-		$signup_username = $_POST["username"];
-		$check = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
+		if($username != $sanitized_username){
+			$arr = array('response' => 'Failure', 'message' => 'Username cannot contain special characters.');
+			echo json_encode($arr);
+			break;
+		}
+		// Request username from database
+		$check = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$username'");
 		$check_row = mysqli_fetch_array($check);
-		if($check_row["Username"] == $signup_username) {
+
+		// Return error if provided signup username exsists in database
+		if($check_row["Username"] == $username) {
 			$arr = array('response' => 'Failure', 'message' => 'Username already exsists, please use different username.');
 			echo json_encode($arr);
 		}
 		break;
+	/**
+	* Will attempt to create new account using provided signup username and password
+	*/
 	case "signup":
-		$signup_username = $_POST["username"];
-		$signup_password = $_POST["password"];
-
-		if(empty($signup_username) || empty($signup_password)) {
+		// Verifies no fields are empty
+		if(empty($username) || empty($password)) {
 			$arr = array('response' => 'Failure', 'message' => 'Username and password cannot be empty.');
 			echo json_encode($arr);
+			break;
+		} 
+		
+		$check = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$username'");
+		$check_row = mysqli_fetch_array($check);
+		
+		// Return error if provided signup username exsists in database
+		if($check_row["Username"] == $username) {
+			$arr = array('response' => 'Failure', 'message' => 'Username already exsists, please use different username.');
+			echo json_encode($arr);
+			break;
 		} else {
-			$check = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
-			$check_row = mysqli_fetch_array($check);
-			if($check_row["Username"] == $signup_username) {
-				$arr = array('response' => 'Failure', 'message' => 'Username already exsists, please use different username.');
+		// Hash password and attempt to create user account
+			$hashpw = password_hash($password, PASSWORD_DEFAULT);
+			$insert = mysqli_query($con,"INSERT INTO table1 (Username, Password, Role) VALUES ('$username', '$hashpw','user')");
+			$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$username'");
+			$row = mysqli_fetch_array($result);
+
+			// Verifies against database that account was created.
+			if($row["Username"] == $username) {
+				$arr = array('response' => 'Success', 'message' => 'Your account was created successfully.');
 				echo json_encode($arr);
 			} else {
-				$hashpw = password_hash($signup_password, PASSWORD_DEFAULT);
-				$insert = mysqli_query($con,"INSERT INTO table1 (Username, Password, Role) VALUES ('$signup_username', '$hashpw','user')");
-				$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$signup_username'");
-				$row = mysqli_fetch_array($result);
-
-				if($row["Username"] == $signup_username) {
-					$arr = array('response' => 'Success', 'message' => 'Your account was created successfully.');
-					echo json_encode($arr);
-				} else {
-					$arr = array('response' => 'Failure', 'message' => 'Unable to create your account, Please try again.');
-					echo json_encode($arr);
-				}
+				$arr = array('response' => 'Failure', 'message' => 'Unable to create your account, Please try again.');
+				echo json_encode($arr);
 			}
 		}
-			break;
-	case "login":
-		$form_username = $_POST["username"];
-		$form_password = $_POST["password"];
-	
-		// Select the database to use
-		$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$form_username'");
+		break;
+	/**
+	* Compares username and password against database
+	*/
+	case "login":	
+		$result = mysqli_query($con,"SELECT * FROM table1 WHERE Username = '$username'");
 		$row = mysqli_fetch_array($result);
-
-		if ($row["Username"] == $form_username && password_verify($form_password, $row["Password"])) {
+		
+		if ($row["Username"] == $username && password_verify($password, $row["Password"])) {
 			$arr = array('response' => 'Success', 'message' => 'You are a validated user.');
 			echo json_encode($arr);
 		} else {
