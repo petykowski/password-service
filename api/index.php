25a7501283034b79be7056c6b8ff5a81d9a55cae
Sean Petykowski <spetykowski@gmail.com>
2017-03-14 19:07:25 -0400

    Code Cleanup
    This commit moves all logic from "action" switch/case to functions. This
    allows for improved legibility and re-usability.

--- api/index.php
+++ api/index.php
@@ -6,66 +6,66 @@ header('Content-Type: application/json');
 // Access provided by database configuration file
 require_once dirname(__DIR__).'/config.php';
 
-// Format and Sanitize Username
-$username = strtolower($_POST["username"]);
-$sanitized_username = filter_var($username, FILTER_SANITIZE_EMAIL);
-
-$password = $_POST["password"];
-
-$user_id = $_POST["user_id"];
-
 //Connect to Database
 $con = new mysqli($servername, $sqlusername, $sqlpassword, $sqldbname)
 	or die("Unable to connect to MySQL");
 	
 switch ($_POST["action"]) {
-	/**
-	* Will check the provided signup username against the user database to check for exsisting entries.
-	*/
 	case "validate_username":
-		if($username != $sanitized_username){
-			$arr = array('response' => 'Failure', 'message' => 'Username cannot contain special characters.');
-			echo json_encode($arr);
-			break;
-		}
-		// Request username from database
-		$check = mysqli_query($con,"SELECT * FROM USERS WHERE EMAIL = '$username'");
-		$check_row = mysqli_fetch_array($check);
-
-		// Return error if provided signup username exsists in database
-		if($check_row["EMAIL"] == $username) {
-			$arr = array('response' => 'Failure', 'message' => 'Username already exsists, please use different username.');
-			echo json_encode($arr);
-		}
+		validateUsername($con, $_POST["username"]);
 		break;
-	/**
-	* Will attempt to create new account using provided signup username and password
-	*/
+		
 	case "signup":
+		createUser($con, $_POST["username"], $_POST["password"]);
+		break;
+
+	case "login":
+		validateUserCredentials($con, $_POST["username"], $_POST["password"]);
+		break;
+		
+	case "profile":	
+		retrieveUserProfile($con, $_POST["user_id"]);
+		break;
+
+	case "update-profile":	
+		updateUserProfile($con, $_POST["user_id"], $_POST["fullname"], $_POST["bio"]);
+		break;
+}
+
+/**
+* Creates a New User
+* $con: The SQL Server where the query will be executed.
+* $user_name: The username to be assoicated to new account.
+* $password: The password assoicated to the new account.
+*/
+function createUser($con, $user_name, $password)
+{
 		// Verifies no fields are empty
-		if(empty($username) || empty($password)) {
+		if(empty($user_name) || empty($password)) {
 			$arr = array('response' => 'Failure', 'message' => 'Username and password cannot be empty.');
 			echo json_encode($arr);
 			break;
 		} 
 		
-		$check = mysqli_query($con,"SELECT * FROM USERS WHERE EMAIL = '$username'");
+		$sanitized_user_name = filter_var(strtolower($user_name), FILTER_SANITIZE_EMAIL);
+		
+		$check = mysqli_query($con,"SELECT * FROM USERS WHERE EMAIL = '$sanitized_user_name'");
 		$check_row = mysqli_fetch_array($check);
 		
 		// Return error if provided signup username exsists in database
-		if($check_row["EMAIL"] == $username) {
+		if($check_row["EMAIL"] == $sanitized_user_name) {
 			$arr = array('response' => 'Failure', 'message' => 'Username already exsists, please use different username.');
 			echo json_encode($arr);
 			break;
 		} else {
-		// Hash password and attempt to create user account
-			$hashpw = password_hash($password, PASSWORD_DEFAULT);
-			$insert = mysqli_query($con,"INSERT INTO USERS (EMAIL, PASSWORD) VALUES ('$username', '$hashpw')");
-			$result = mysqli_query($con,"SELECT * FROM USERS WHERE EMAIL = '$username'");
+			// Hash password and attempt to create user account
+			$hashpw = password_hash($pass_word, PASSWORD_DEFAULT);
+			$insert = mysqli_query($con,"INSERT INTO USERS (EMAIL, PASSWORD) VALUES ('$sanitized_user_name', '$hashpw')");
+			$result = mysqli_query($con,"SELECT * FROM USERS WHERE EMAIL = '$sanitized_user_name'");
 			$row = mysqli_fetch_array($result);
 
 			// Verifies against database that account was created.
-			if($row["EMAIL"] == $username) {
+			if($row["EMAIL"] == $sanitized_user_name) {
 				$arr = array('response' => 'Success', 'message' => 'Your account was created successfully.');
 				echo json_encode($arr);
 			} else {
@@ -73,44 +73,91 @@ switch ($_POST["action"]) {
 				echo json_encode($arr);
 			}
 		}
-		break;
-	/**
-	* Compares username and password against database
-	*/
-	case "login":	
-		$result = mysqli_query($con,"SELECT * FROM USERS WHERE EMAIL = '$username'");
+}
+
+/**
+* Determines If Provided Username Already Exsists
+* $con: The SQL Server where the query will be executed.
+* $user_name: The username attempting to login.
+*/
+function validateUsername($con, $user_name)
+{
+		$sanitized_user_name = filter_var(strtolower($user_name), FILTER_SANITIZE_EMAIL);
+		if($user_name != $sanitized_user_name){
+			$arr = array('response' => 'Failure', 'message' => 'Username cannot contain special characters.');
+			echo json_encode($arr);
+			break;
+		}
+		// Request username from database
+		$check = mysqli_query($con,"SELECT * FROM USERS WHERE EMAIL = '$user_name'");
+		$check_row = mysqli_fetch_array($check);
+
+		// Return error if provided signup username exsists in database
+		if($check_row["EMAIL"] == $sanitized_user_name) {
+			$arr = array('response' => 'Failure', 'message' => 'Username already exsists, please use different username.');
+			echo json_encode($arr);
+		}
+}
+
+/**
+* Determines If Provided Username and Password Are Valid
+* $con: The SQL Server where the query will be executed.
+* $user_name: The username attempting to login.
+* $password: The password assoicated to the login username.
+*/
+function validateUserCredentials($con, $user_name, $password)
+{
+		$user_name = filter_var(strtolower($user_name), FILTER_SANITIZE_EMAIL);
+		$result = mysqli_query($con,"SELECT * FROM USERS WHERE EMAIL = '$user_name'");
 		$row = mysqli_fetch_array($result);
 		
-		if ($row["EMAIL"] == $username && password_verify($password, $row["PASSWORD"])) {
+		if ($row["EMAIL"] == $user_name && password_verify($password, $row["PASSWORD"])) {
 			$arr = array('response' => 'Success', 'message' => 'You are a validated user.', 'user_id' => $row["USER_ID"]);
 			echo json_encode($arr);
 		} else {
 			$arr = array('response' => 'Failure', 'message' => 'Sorry, your credentials are not valid, Please try again.');
 			echo json_encode($arr);
 		}
-		break;
-		
-	case "profile":	
-		$result = mysqli_query($con,"SELECT * FROM PROFILE WHERE FK_USER_ID = '$user_id'");
+}
+
+/**
+* Returns All User Profile Details From Database
+* $con: The SQL Server where the query will be executed.
+* $id: The User ID of the profile to be returned.
+*/
+function retrieveUserProfile($con, $id)
+{
+		$result = mysqli_query($con,"SELECT * FROM PROFILE WHERE FK_USER_ID = '$id'");
 		$row = mysqli_fetch_array($result);
 		
-		$arr = array('response' => 'Success', 'fullname' => $row["FULL_NAME"], 'bio' => $row["BIO"]);
-		echo json_encode($arr);
-		break;
-		
-	case "update-profile":	
-		$fullname = $_POST["fullname"];
-		$fullname = $con->real_escape_string($fullname);
-		$bio = $_POST["bio"];
-		$bio = $con->real_escape_string($bio);
-		$res = mysqli_query($con,"UPDATE PROFILE SET FULL_NAME='$fullname', BIO='$bio' WHERE FK_USER_ID=$user_id");
-		if ($res) {
-			$arr = array('response' => 'Success', 'fullname' => $fullname, 'bio' => $bio, 'user_id' => $user_id, 'query' => $res);
+		if ($result) {
+			$arr = array('response' => 'Success', 'message' => 'Returning details of user profile.', 'fullname' => $row["FULL_NAME"], 'bio' => $row["BIO"]);
 			echo json_encode($arr);
 		} else {
-			$arr = array('response' => 'Error', 'fullname' => $fullname, 'bio' => $bio, 'user_id' => $user_id, 'query' => $res);
+			$arr = array('response' => 'Failure', 'message' => 'Unable to connect to server.');
 			echo json_encode($arr);
 		}
-		break;
 }
+
+/**
+* Updates User Profile Details on Database
+* $con: The SQL Server where the query will be executed.
+* $id: The User ID of the profile to be updated.
+* $full_name: The full name of the user.
+* $bio: A short description of the user.
+*/
+function updateUserProfile($con, $id, $full_name, $bio)
+{
+		$full_name = $con->real_escape_string($full_name);
+		$bio = $con->real_escape_string($bio);
+		$res = mysqli_query($con,"UPDATE PROFILE SET FULL_NAME='$full_name', BIO='$bio' WHERE FK_USER_ID=$id");
+		if ($res) {
+			$arr = array('response' => 'Success', 'fullname' => $full_name, 'bio' => $bio, 'user_id' => $user_id, 'query' => $res);
+			echo json_encode($arr);
+		} else {
+			$arr = array('response' => 'Error', 'fullname' => $full_name, 'bio' => $bio, 'user_id' => $user_id, 'query' => $res);
+			echo json_encode($arr);
+		}
+}
+
 ?>
